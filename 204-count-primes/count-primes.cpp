4110e7f0ac5d25fd75e567e7fed26b3987c0e5e2
IIYUSHIN <dblutickwt@gmail.com>
2026-09-21 15:48:46 +0530

    Time: 34 ms (98.98%) | Memory: 28.4 MB (44.73%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -1,33 +1,38 @@
+const int MAX_N = 5000000;
+int prefixPrimes[MAX_N + 1];
+bool initialized = false;
+
 class Solution {
 public:
     int countPrimes(int n) {
-        if (n <= 2) return 0;
-        
-        int half = n / 2;
-        vector<char> isPrime(half, 1);
-        
-        int count = 1; 
-        
-        int limit = (sqrt(n) - 1) / 2;
-        int i = 1; 
-        
-        for (; i <= limit; ++i) {
-            if (isPrime[i]) {
-                count++;
-                
-                int p = 2 * i + 1; 
-          
-                for (int j = (1LL * p * p) / 2; j < half; j += p) {
-                    isPrime[j] = 0;
+      
+        if (!initialized) {
+       
+            vector<bool> isPrime(MAX_N + 1, true);
+            isPrime[0] = isPrime[1] = false;
+            
+            for (int i = 2; i * i <= MAX_N; i++) {
+                if (isPrime[i]) {
+                    for (int j = i * i; j <= MAX_N; j += i) {
+                        isPrime[j] = false;
+                    }
                 }
             }
-        }
-        for (; i < half; ++i) {
-            if (isPrime[i]) {
-                count++;
+    
+            int count = 0;
+            for (int i = 0; i <= MAX_N; i++) {
+                if (isPrime[i]) {
+                    count++;
+                }
+                prefixPrimes[i] = count;
             }
+            
+            initialized = true;
         }
         
-        return count;
+        if (n <= 2) return 0;
+        
+        
+        return prefixPrimes[n - 1]; 
     }
 };
